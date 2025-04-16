1b45f75508498368e210e05c6f83cc4a7ae50cad
GaufreGentille <raphimignon@gmail.com>
2025-04-16 17:12:03 +0200

    Update emote_formatter.js

--- emote_formatter.js
+++ emote_formatter.js
@@ -1,41 +1,22 @@
 // emote_formatter.js
 
 export function formatEmotes(text) {
-    // Liste d'emotes supportées sur la chaîne
     const emoteList = [
         'Kappa',
         'OMEGALUL',
         'PogChamp',
+        'gaufreLol',
         'LUL',
+        'PepeHands',
         'BibleThump',
         '4Head',
         'FeelsStrongMan',
         'KEKW',
         'monkaS',
-        'gaufre1Ffee',
-        'gaufre1Justice',
-        'gaufre1Gunner',
-        'gaufre1Wut',
-        'bongoTap',
-        'catJAM',
-        'catKISS',
-        'HUH',
-        'Jigglin',
-        'PogTasty',
-        'PETTHEMODS',
-        'pedro',
-        'OMEGALUL',
-        'muted',
-        'LICKA',
-        'POLICE',
-        'RobustoAPT',
-        'ThisIsFine',
-        'VIBE',
-        'Joel',        
         'gachiHYPER'
     ];
 
-     emoteList.forEach(emote => {
+    emoteList.forEach(emote => {
         const regex = new RegExp(`:${emote}:`, 'gi');
         text = text.replace(regex, emote);
     });
@@ -44,45 +25,25 @@ export function formatEmotes(text) {
 }
 
 export function addRandomEmoteToEnd(text) {
-    const emoteList = [        
+    const emoteList = [
         'Kappa',
         'OMEGALUL',
         'PogChamp',
+        'gaufreLol',
         'LUL',
-        'BibleThump',
-        '4Head',
+        'PepeHands',
         'FeelsStrongMan',
-        'KEKW',
-        'monkaS',
-        'gaufre1Ffee',
-        'gaufre1Justice',
-        'gaufre1Gunner',
-        'gaufre1Wut',
-        'bongoTap',
-        'catJAM',
-        'catKISS',
-        'HUH',
-        'Jigglin',
-        'PogTasty',
-        'PETTHEMODS',
-        'pedro',
-        'OMEGALUL',
-        'muted',
-        'LICKA',
-        'POLICE',
-        'RobustoAPT',
-        'ThisIsFine',
-        'VIBE',
-        'Joel',        
-        'gachiHYPER'
+        'KEKW'
     ];
-    
+
     // Supprimer ponctuation finale s'il y en a
     text = text.trim().replace(/[.!?]+$/, '');
 
     // Ajouter une emote random à la fin
     const randomEmote = emoteList[Math.floor(Math.random() * emoteList.length)];
     return `${text} ${randomEmote}`;
+}
+
     
     // Remplacer :emote: par emote
     emoteList.forEach(emote => {
