e314cf275687d1e92553160d24f7dda06a594f2d
GaufreGentille <raphimignon@gmail.com>
2025-04-16 17:00:59 +0200

    Update emote_formatter.js

--- emote_formatter.js
+++ emote_formatter.js
@@ -35,6 +35,55 @@ export function formatEmotes(text) {
         'gachiHYPER'
     ];
 
+     emoteList.forEach(emote => {
+        const regex = new RegExp(`:${emote}:`, 'gi');
+        text = text.replace(regex, emote);
+    });
+
+    return text;
+}
+
+export function addRandomEmoteToEnd(text) {
+    const emoteList = [        
+        'Kappa',
+        'OMEGALUL',
+        'PogChamp',
+        'LUL',
+        'BibleThump',
+        '4Head',
+        'FeelsStrongMan',
+        'KEKW',
+        'monkaS',
+        'gaufre1Ffee',
+        'gaufre1Justice',
+        'gaufre1Gunner',
+        'gaufre1Wut',
+        'bongoTap',
+        'catJAM',
+        'catKISS',
+        'HUH',
+        'Jigglin',
+        'PogTasty',
+        'PETTHEMODS',
+        'pedro',
+        'OMEGALUL',
+        'muted',
+        'LICKA',
+        'POLICE',
+        'RobustoAPT',
+        'ThisIsFine',
+        'VIBE',
+        'Joel',        
+        'gachiHYPER'
+    ];
+    
+    // Supprimer ponctuation finale s'il y en a
+    text = text.trim().replace(/[.!?]+$/, '');
+
+    // Ajouter une emote random à la fin
+    const randomEmote = emoteList[Math.floor(Math.random() * emoteList.length)];
+    return `${text} ${randomEmote}`;
+    
     // Remplacer :emote: par emote
     emoteList.forEach(emote => {
         const regex = new RegExp(`:${emote}:`, 'gi');
